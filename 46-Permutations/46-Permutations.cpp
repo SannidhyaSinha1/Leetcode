969a70d7f0d7b5442dbe50ee456bc396241651e3
Sannidhya Sinha <sannidhyasinha1309@gmail.com>
2026-05-27 16:31:10 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,28 @@
+// Last updated: 27/05/2026, 16:31:09
+class Solution {
+public:
+    void getPermutations(vector<int>&nums,int idx,vector<vector<int>>&ans){
+        if(idx==nums.size()){
+            ans.push_back({nums});
+            return;
+
+        }
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[idx],nums[i]);
+            getPermutations(nums,idx+1,ans);
+
+            //backtracking
+            swap(nums[idx],nums[i]);
+
+        }
+        
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        getPermutations(nums,0,ans);
+        
+        return ans;
+    }
+};
