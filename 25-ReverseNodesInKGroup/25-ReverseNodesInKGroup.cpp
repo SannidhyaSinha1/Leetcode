0048020a8b4f37bd4c86efd9a3f1e85612b77c45
Sannidhya Sinha <sannidhyasinha1309@gmail.com>
2026-05-28 13:54:07 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.cpp
@@ -0,0 +1,48 @@
+// Last updated: 28/05/2026, 13:54:06
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        ListNode *temp= head;
+        int count =0;
+
+        while(count<k){
+            if(temp==NULL){
+                return head;
+            }
+            temp=temp->next;
+            count++;
+
+        }
+
+        //recursive call for rest of LL
+
+        ListNode *prevNode=reverseKGroup(temp, k);
+
+        //reverse the List
+        temp=head;
+        count=0;
+        while(count<k){
+            ListNode* next =temp->next;
+            temp->next = prevNode;
+            prevNode=temp;
+            temp=next;
+            count++;
+
+
+        }
+        return prevNode;
+
+        
+    }
+};
