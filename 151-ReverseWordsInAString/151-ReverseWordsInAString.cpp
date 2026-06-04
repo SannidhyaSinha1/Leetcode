ccee2c601274405f3e8369da007d77f93d87b046
Sannidhya Sinha <sannidhyasinha1309@gmail.com>
2026-06-04 14:23:39 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,36 @@
+// Last updated: 04/06/2026, 14:23:38
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+
+        
+        string ans="";
+
+        for(int i=0;i<s.length();i++){
+            string word="";
+
+            while(i<s.length() && s[i]!=' '){
+                word+=s[i];
+                i++;
+
+
+            }
+            reverse(word.begin(),word.end());
+
+            
+
+            if(word.length()>0){
+                ans+=" "+word;
+                
+
+            }
+            
+
+
+        }
+        return ans.substr(1);
+        
+        
+    }
+};
